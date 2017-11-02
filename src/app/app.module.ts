407bbf6616de94d10a3c9b4f7e8589ab524055b9
Zelloufi Ait Oumeri Ilyas <LM5615@sdmn01.sirius.infra.com>
2017-11-02 16:49:23 +0100

    bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule, ErrorHandler } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
-
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import { AboutPage } from '../pages/about/about';
 import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
