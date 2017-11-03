70b78b85bca4823fb05b48ee090e2b80c2336d03
zelloufi <zelloufi.ilyas@gmail.com>
2017-11-03 07:47:57 +0100

    http

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,17 +1,42 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import {FormBuilder, FormControl, FormGroup, Validators} from "@angular/forms";
+import {PagesService} from "../pages.services";
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  private signupForm: FormGroup;
 
-  constructor(public navCtrl: NavController) {
-
+  constructor(public navCtrl: NavController, private fb: FormBuilder, private pageService: PagesService) {
+    this.signupForm = this.buildForm();
   }
 
   register() {
-    alert("register")
+    const data = {
+      firstname: this.signupForm.controls.firstname.value,
+      lastname: this.signupForm.controls.lastname.value,
+      username: this.signupForm.controls.username.value,
+      email: this.signupForm.controls.email.value,
+      password: this.signupForm.controls.password.value,
+      password_confirmation: this.signupForm.controls.password_confirmation.value,
+      avatar: this.signupForm.controls.avatar.value,
+    };
+    console.log(data)
+    this.pageService.signup(data);
+  }
+
+  buildForm() {
+    return this.fb.group({
+      firstname: new FormControl(null),
+      lastname: new FormControl(null),
+      username: new FormControl(null),
+      email: new FormControl(null),
+      password: new FormControl(null),
+      password_confirmation: new FormControl(null),
+      avatar: new FormControl(null)
+    });
   }
 }
--- src/pages/pages.services.ts
+++ src/pages/pages.services.ts
@@ -2,20 +2,34 @@ import {Injectable} from "@angular/core";
 import {Http, Headers, RequestOptions} from '@angular/http';
 import 'rxjs/add/operator/catch';
 import 'rxjs/add/operator/toPromise';
+import {Observable} from "rxjs/Observable";
+import "rxjs/add/observable/fromPromise";
 
 @Injectable()
 export class PagesService {
-  const
-  API_URl = "http://memoriz-api.herokuapp.com"
-  const
-  headers = new Headers(
-    {
-      'Content-Type': 'application/json'
-    });
-  let
-  options = new RequestOptions({headers: headers});
+  constructor(private http: Http) {
+  }
 
-  signUp(data) {
+  const API_URL = "http://memoriz-api.herokuapp.com/";
+  const headers = new Headers({'Content-Type': 'application/json'});
+  const options = new RequestOptions({headers: this.headers});
 
+  signup(data): Observable<any> {
+    let signupObservable = Observable.fromPromise(new Promise((resolve, reject) => {
+      this.http.post(this.API_URL + 'users/sign_up', data, this.options)
+        .toPromise()
+        .then((response) =>
+        {
+          console.log('API Response : ', response.json());
+          resolve(response.json());
+        })
+        .catch((error) =>
+        {
+          console.error('API Error : ', error.status);
+          console.error('API Error : ', JSON.stringify(error));
+          reject(error.json());
+        });
+    }));
   }
 }
+
